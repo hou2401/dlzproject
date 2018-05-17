4d2214f646411c2f057739c3f65fdc357d58d39b
1531711515@qq.com <1531711515@qq.com>
2018-05-17 22:07:37 +0800

    实体

---
+++ parent/han/src/main/java/com/jk/service/GeneraService.java
@@ -0,0 +1,11 @@
+package com.jk.service;
+
+import com.jk.pojo.Genera;
+
+import java.util.List;
+
+public interface GeneraService {
+    List queryGenera();
+
+    void addGenera(Genera genera);
+}
